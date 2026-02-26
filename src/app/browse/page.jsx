6036902b1be459bfe9f426eab5582fc7a29b2247
krobbins2-sbcsd <009163720@coyote.csusb.edu>
2026-02-26 14:28:14 -0800

    CSS

--- src/app/browse/page.jsx
+++ src/app/browse/page.jsx
@@ -4,26 +4,57 @@ import "tailwindcss/base.css"
 import "tailwindcss/utilities.css"
 import "tailwindcss/components.css"
 import "tailwindcss/variants.css"
-import "tailwindcss/screens.css"
 
 
 export default function Home() {
   return (
-  <div>
+  <div className="bg-emerald-50 min-h-screen min-w-screen object-fill">
 
-    <div>
-      <img className="w-screen h-250 object-fill p-0" width="screen" height="250" src="https://thumbs.dreamstime.com/b/day-life-pixelated-farm-dive-vibrant-world-charming-pixel-art-where-characters-tend-to-their-crops-332448133.jpg"/>
+    <div className="w-screen h-90">
+      <img className="w-screen max-h-80" src="https://thumbs.dreamstime.com/b/day-life-pixelated-farm-dive-vibrant-world-charming-pixel-art-where-characters-tend-to-their-crops-332448133.jpg"/>
     </div>
     
-    <div className="flex p-4 justify-center gap-x-600">
-      <div className="">
-      <h1 className="text-3xl font-bold text-center mt-4">fresh2table</h1>
+    <div className="flex justify-between items-center w-full p-4 bg-green-200">
+      <div className="flex w-1/3"></div>
+      <div className="w-1/3 justify-center items-center flex">
+      <span className="text-3xl font-serif font-bold text-center mt-4 text-emerald-900">fresh2table</span>
       </div>
-      <div className="">
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 ml-4">
-        Sign Up
+      <div className="flex justify-end w-1/3">
+      <button className="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded-full mt-4 ml-4 h-10 w-10">
+        ?
       </button>
+      <button className="bg-emerald-800 rounded-full mt-3 ml-4 w-12 h-12"></button>
       </div>
+    </div>
+
+    <div className = "flex justify-between items-center w-full p-4">
+    <div className = "flex w-1/6">
+    <span>
+      <a href>
+      <button className="bg-emerald-400 hover:bg-emerald-500 text-white font-bold rounded-full mt-4 ml-4 h-20 w-40">
+        Browse
+      </button>
+      </a>
+      </span>
+    </div>
+    <div className = "flex w-1/6 justify-center">
+    <span>
+      <a href>
+      <button className="bg-emerald-400 hover:bg-emerald-500 text-white font-bold rounded-full mt-4 ml-4 h-20 w-40">
+        Cart
+      </button>
+      </a>
+      </span>
+    </div>
+    <div className = "flex w-1/6 justify-end p-1">
+    <span>
+      <a href>
+      <button className="bg-emerald-400 hover:bg-emerald-500 text-white font-bold rounded-full mt-4 ml-4 h-20 w-40">
+        Orders
+      </button>
+      </a>
+      </span>
+    </div>
     </div>
 
   </div>
