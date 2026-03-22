5c1d03b755c1e98a10064909d723f2561c05a5c6
krobbins2-sbcsd <009163720@coyote.csusb.edu>
2026-03-22 11:01:22 -0700

    hella build
    profile features, added new image files, changes made to components for easier access

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
+import Browse from "./browse/page";
+
+
 export default function Home() {
   return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>Food2Table Test Home</h1>
-      <p>If you see this, it is working from <code>src/app</code>.</p>
-
-    </main>
+    <div>
+    <Browse />
+    </div>
   );
 }
